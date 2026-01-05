59065d8814ea48ec3fed7e396da11225136f3096
vinodwagh07 <vinodwagh.rcpit2003@gmail.com>
2026-01-05 18:54:30 +0530

    chore(constants): update navLink.js for navigation pages

--- src/constants/navLink.js
+++ src/constants/navLink.js
@@ -1,7 +1,7 @@
 export const navLinks = [
-  { name: "SHOP", href: "/shops" },
+  { name: "SHOP", href: "/" },
   { name: "SKILLS", href: "/skills" },
   { name: "STORIES", href: "/stories" },
   { name: "ABOUT", href: "/about" },
-  { name: "CONTACT US", href: "/contact" },
+  { name: "CONTACT US", href: "/contact-us" },
 ];
