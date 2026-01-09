29fe21d630ee5642696ec03dafa9ea167a9c4a74
vinodwagh07 <vinodwagh.rcpit2003@gmail.com>
2026-01-09 12:35:52 +0530

    feat(product-listing): add FilterBar component with toggle functionality

--- src/components/ProductListing/FilterBar.js
+++ src/components/ProductListing/FilterBar.js
@@ -1,7 +1,46 @@
-export default function FiltersBar() {
+"use client";
+
+import styles from "./FilterBar.module.css";
+import { ChevronLeftIcon } from "@heroicons/react/24/solid";
+import { ChevronRightIcon } from "@heroicons/react/24/solid";
+import { useState } from "react";
+import SortDropdown from "./SortDropdown";
+
+export default function FiltersBar({ count, showFilters, toggleFilters }) {
+  const [sortBy, setSortBy] = useState("recommended");
+
   return (
-    <section>
-      <p>Filters Bar</p>
+    <section className={styles.filterSection} >
+      <div className={styles.filterControls}>
+        <div className={styles.controlsLeft}>
+          <span className={styles.itemsCount}>{count} ITEMS</span>
+
+          <button
+            onClick={toggleFilters}
+            className={styles.toggleBtn}
+            aria-expanded={showFilters}
+            aria-label={showFilters ? "Hide filters" : "Show filters"}
+          >
+            {showFilters ? (
+              <ChevronLeftIcon className={styles.iconSm} />
+            ) : (
+              <ChevronRightIcon className={styles.iconSm} />
+            )}
+            {showFilters ? "HIDE FILTER" : "SHOW FILTER"}
+          </button>
+
+          <button
+            className={styles.mobileFilterBtn}
+            onClick={toggleFilters}
+          >
+            FILTER
+          </button>
+        </div>
+        {/* SORT FILTERS AND OPTIONS */}
+        <div className={styles.controlsRight}>
+          <SortDropdown selectedValue={sortBy} onSelect={setSortBy} />
+        </div>
+      </div>
     </section>
   );
 }
