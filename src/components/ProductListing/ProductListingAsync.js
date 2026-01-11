1d9d60d164cacf8241d3cf7af2f8b7ed20efb60e
vinodwagh07 <vinodwagh.rcpit2003@gmail.com>
2026-01-11 14:51:50 +0530

    fix: resolve SSR product fetching and Suspense streaming issue

--- src/components/ProductListing/ProductListingAsync.js
+++ src/components/ProductListing/ProductListingAsync.js
@@ -1,7 +1,7 @@
 import ProductListing from "./index";
 import { getProducts } from "@/lib/api";
 
-export default async function ProductListingAsync({ promise }) {
+export default async function ProductListingAsync() {
   const products = await getProducts();
   return <ProductListing products={products} />;
 }
